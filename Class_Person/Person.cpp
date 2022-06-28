9a5a29f1f52e1fdcac1baedfdd06294aaffec454
yuraua21 <105919483+yuraua21@users.noreply.github.com>
2022-06-28 13:01:52 +0300

    Add files via upload

---
+++ Class_Person/Person.cpp
@@ -0,0 +1,25 @@
+﻿#pragma once  
+#include <iostream>
+using namespace std;
+
+class Person    
+{
+private:
+    string name;  
+    int age;
+    int weight;
+
+public:
+    Person(const Person& pers1);
+   
+    
+    char* getname(); 
+    int getage();
+    int getweight();
+  
+    Person operator=(Person op2);
+    friend ostream &operator<<(ostream &stream, Person obj); 
+    friend istream &operator>>(istream &stream, Person &obj); 
+
+
+};
---
+++ Class_Person/Person.h
@@ -0,0 +1,63 @@
+﻿#include "Person.h"
+using namespace std;
+
+
+Person::Person() 
+{
+    name = "";  
+    int age = 0;
+    int weight=0;
+}
+
+
+Person::Person(const Person& pers1) {
+    Person(pers1.name, pers1.age, pers1.weight);
+{
+}
+
+
+
+
+
+string Person::getname() 
+    return name;
+}
+
+int Person::getage() 
+{
+    return age;
+}
+
+int Person::getweight() 
+{
+    return weight;
+}
+
+
+
+Person Person::operator=(Person op2) 
+{
+    name = op2.name;
+    age = op2.age;
+    weight = op2.weight;
+
+    return *this;
+}
+
+
+ostream &operator<<(ostream &stream, Person obj) 
+{ 
+  stream << "Name : " << obj.name << ", ";
+  stream << "Age : " << obj.age << ", ";
+  stream << "Weight : " << obj.weight << " \n ";
+
+  return stream; 
+}
+
+istream &operator>>(istream &stream, Person &obj) 
+{
+  cout << "Enter name, age, weight: ";
+  stream >> obj.name >> obj.age >> obj.weight >>;
+
+  return stream;
+}
